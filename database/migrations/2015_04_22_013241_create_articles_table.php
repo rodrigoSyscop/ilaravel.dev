c737ff694ae2402ea54dad4e16d173527e313c04
Rodrigo de Lima Vieira <rodrigodelimavieira@gmail.com>
2015-04-21 22:50:39 -0300

    Database schema
    Criando schema do banco de dados.
    Repare que a tabela users já foi criada pelo Laravel, apenas adiciono
    softDeletes.

---
+++ database/migrations/2015_04_22_013241_create_articles_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArticlesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('articles', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->integer('user_id')->unsigned();
+			$table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+			$table->integer('status_id')->unsigned();
+			$table->foreign('status_id')->references('id')->on('statuses')->onDelete('cascade');
+			$table->string('title');
+			$table->string('slug');
+			$table->text('excerpt');
+			$table->text('content');
+			$table->softDeletes();
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('articles');
+	}
+
+}
