72b1da12db98be3a4407b87a8fe3e51e4f73f070
Rodrigo de Lima Vieira <rodrigodelimavieira@gmail.com>
2015-04-22 20:11:38 -0300

    Adicionando tabela pivo
    Adicionando tabela pivo para relacionamento many-to-many entre articles
    e tags.

--- database/migrations/2015_04_22_135902_create_article_tag_table.php
+++ database/migrations/2015_04_22_135902_create_article_tag_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateArticleTabTable extends Migration {
+class CreateArticleTagTable extends Migration {
 
 	/**
 	 * Run the migrations.
@@ -18,7 +18,6 @@ class CreateArticleTabTable extends Migration {
 			$table->foreign('article_id')->references('id')->on('articles')->onDelete('cascade');
 			$table->integer('tag_id')->unsigned();
 			$table->foreign('tag_id')->references('id')->on('tags')->onDelete('cascade');
-			$table->timestamps();
 		});
 	}
 
